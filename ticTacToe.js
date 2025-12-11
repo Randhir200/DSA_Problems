5e18aedbaff1f55a633a7b69f4f26517c5a16100
Randhir Kumar <92780081+Randhir200@users.noreply.github.com>
2025-12-11 16:55:20 +0000

    skeleton logic added

---
+++ ticTacToe.js
@@ -0,0 +1,26 @@
+const mat = [
+  ['0', '1', '0'],
+  ['0', '1', '0'],
+  ['0', '0', '1'],
+];
+
+const players = {
+    '0': 'Player 1',
+    '1': 'Player 2'
+}
+
+function traverse() {
+    for(let i=0; i<mat.length; i++){
+        for(let j=0; j<mat[i].length; i++){
+
+        }
+    }
+}
+
+function checkWinner() {
+
+    return players['0']
+}
+
+
+console.log(checkWinner());
